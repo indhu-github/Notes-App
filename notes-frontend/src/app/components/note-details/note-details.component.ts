7b415dd1fd46e9f17c8ec9f120490a0117a93628
indhu-github <indhusworkmail@gmail.com>
2021-03-28 21:12:43 +0530

    added note details page

--- notes-frontend/src/app/components/note-details/note-details.component.ts
+++ notes-frontend/src/app/components/note-details/note-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-note-details',
@@ -10,13 +11,14 @@ import { Note } from 'src/app/shared/note.model';
 export class NoteDetailsComponent implements OnInit {
 
   note:Note;
-  constructor() { }
+  constructor(private noteService:NotesService) { }
 
   ngOnInit(): void {
     this.note=new Note();
   }
 
   onSubmit(form:NgForm){
-    console.log(form);
+   // console.log(form);
+   this.noteService.add(form.value);
   }
 }
---
+++ notes-frontend/src/app/shared/notes.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Note } from './note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+  notes:Note[]=new Array<Note>();
+
+  constructor() { }
+
+  get(id:number){
+    return this.notes[id];
+  }
+
+  getId(note:Note){
+    return this.notes.indexOf(note);
+  }
+
+  add(note:Note){
+    return this.notes.push(note);
+  }
+
+  update(id:number,title:string,body:string){
+    let note=this.notes[id];
+    note.title=title;
+    note.body=body;
+  }
+
+  delete(id:number){
+    return this.notes.splice(id,1);
+  }
+}
